c510a2364ed5b0d093c8bbb43dcc089166c736b1
Nathachai Thongniran <inid3a@gmail.com>
2016-08-02 23:48:21 +0700

    Complete

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ module.exports = {
   
   /**
    * [createGrid description]
-   * NOT TESTED
+   * UNUSED
    * 
    * @see http://stackoverflow.com/questions/2250942/javascript-string-matching-pattern-help
    * 
@@ -29,16 +29,60 @@ module.exports = {
   },
 
   /**
-   * [merge2IntersectionString description]
+   * [replaceLastOccurrence description]
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+   * 
+   * @param  {[type]} str     [description]
+   * @param  {[type]} find    [description]
+   * @param  {[type]} replace [description]
+   * @return {[type]}         [description]
+   */
+  replaceLastOccurrence: function(str, find, replace) {
+    var result = str,
+      n = str.lastIndexOf(find);
+
+    result = str.slice(0, n) + str.slice(n).replace(find, replace);
+
+    return result;
+  },
+
+  /**
+   * [mergeIntersection description]
+   * need to be
+   * - intersected
+   * - ordered
    * 
    * @param  {[type]} str1 [description]
    * @param  {[type]} str2 [description]
    * @return {[type]}      [description]
    */
-  merge2IntersectionString: function(str1, str2) {
-    var result = '';
+  mergeIntersection: function(str1, str2, empBeginStr, empAfterStr) {
+    var result = str1;
+    var isStr1ContainStr2 = this.isContainStr(str1, str2);
+    var hasEmp = (empBeginStr && empAfterStr);
+   
+    if (isStr1ContainStr2) {
+      if (hasEmp) {
+        result = str1.replace(str2, empBeginStr + str2 + empAfterStr);
+      }
 
-    return result;  
+    } else {
+      var intersection = this.getIntersection(str1, str2),
+        intersectionPhases = intersection.sequence;
+
+      if (hasEmp) {
+        var tmp1 = this.replaceLastOccurrence(str1, intersectionPhases, empBeginStr + intersectionPhases + empAfterStr),
+          tmp2 = str2.replace(intersectionPhases, '');
+
+        result = tmp1 + tmp2;
+
+      } else {
+        result = this.replaceLastOccurrence(str1, intersectionPhases, '') + str2;
+      }
+    }
+
+    return result;
   },
 
   /**
@@ -97,51 +141,95 @@ module.exports = {
   /**
    * [getIntersection description]
    * 
-   * @see http://stackoverflow.com/questions/2250942/javascript-string-matching-pattern-help
-   * 
    * @param  {[type]} str1 [description]
    * @param  {[type]} str2 [description]
    * @return {[type]}      [description]
    */
   getIntersection: function(str1, str2) {
-    var nStr1 = str1.length,
-      nStr2 = str2.length,
-      grid = this.createGrid(str1.length, str2.length),
-      longest = 0,
-      matches = '',
-      i,
-      j;
+    return this.getLongestCommonSubstring(str1, str2);
+  },
 
-    for (i = 0; i < nStr1; i++) {
-      for (j = 0; j < nStr2; j++) {
-        if (str1.charAt(i) == str2.charAt(j)) {
-          if (i == 0 || j == 0) {
-            grid[i][j] = 1;
+  /**
+   * [longestCommonSubstring description]
+   *
+   * @see https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring
+   * @see http://stackoverflow.com/questions/2250942/javascript-string-matching-pattern-help
+   *  
+   * @param  {[type]} str1 [description]
+   * @param  {[type]} str2 [description]
+   * @return {[type]}      [description]
+   */
+  getLongestCommonSubstring: function(str1, str2) {
+    var result = {
+      length    : 0,
+      sequence  : '',
+      offset    : 0
+    };
+
+    if (!str1 || !str2) return result;
+ 
+    var sequence = '',
+      str1Length = str1.length,
+      str2Length = str2.length,
+      num = new Array(str1Length),
+      maxlen = 0,
+      lastSubsBegin = 0;
+ 
+    for (var i = 0; i < str1Length; i++) {
+      var subArray = new Array(str2Length);
+
+      for (var j = 0; j < str2Length; j++) {
+        subArray[j] = 0;  
+      }
+
+      num[i] = subArray;
+    }
+
+    var thisSubsBegin = 0;
+
+    for (var i = 0; i < str1Length; i++) {
+      for (var j = 0; j < str2Length; j++) {
+        if (str1[i] !== str2[j]) {
+          num[i][j] = 0;  
+
+        } else {
+          if ((i === 0) || (j === 0)) {
+            num[i][j] = 1;
 
           } else {
-            grid[i][j] = grid[i - 1][j - 1] + 1;
+            num[i][j] = 1 + num[i - 1][j - 1];
           }
 
-          if (grid[i][j] > longest) {
-            longest = grid[i][j];
-            matches = '';
-          }
-          
-          if (grid[i][j] == longest) {
-            var match = str1.substring(i - longest + 1, i);
-            matches = match;
+          if (num[i][j] > maxlen) {
+            maxlen = num[i][j];
+            thisSubsBegin = i - num[i][j] + 1;
+
+            // if the current LCS is the same as the last time this block ran
+            if (lastSubsBegin === thisSubsBegin) {
+              sequence += str1[i];
+
+            // this block resets the string builder if a different LCS is found
+            } else {
+              lastSubsBegin = thisSubsBegin;
+              sequence = ''; //clear it
+              sequence += str1.substr(lastSubsBegin, (i + 1) - lastSubsBegin);
+            }
           }
         }
       }
     }
 
-    return matches;
+    var result = {
+      length    : maxlen,
+      sequence  : sequence,
+      offset    : thisSubsBegin
+    };
+
+    return result;
   },
 
   /**
    * [isContainStr description]
-   * NOT TESTED
-   * UNUSED
    * 
    * @see http://stackoverflow.com/questions/1789945/how-to-check-if-one-string-contains-another-substring-in-javascript
    * 
@@ -154,8 +242,27 @@ module.exports = {
   },
 
   /**
+   * [getContainStr description]
+   * 
+   * @param  {[type]} str    [description]
+   * @param  {[type]} needle [description]
+   * @return {[type]}        [description]
+   */
+  getContainStr: function(str, needle) {
+    var result = {
+      length    : needle.length,
+      offset    : -1,
+      isContain : false
+    };
+
+    result.offset = str.indexOf(needle);
+    result.isContain = (result.offset !== -1);
+
+    return result;
+  },
+
+  /**
    * [isArray description]
-   * NOT TESTED
    * 
    * @see http://stackoverflow.com/questions/4775722/check-if-object-is-array
    * 
@@ -168,7 +275,6 @@ module.exports = {
 
   /**
    * [isString description]
-   * NOT TESTED
    *
    * @see http://stackoverflow.com/questions/4059147/check-if-a-variable-is-a-string
    * 
@@ -195,13 +301,29 @@ module.exports = {
       result = arg2,
       tmp = null,
       i = 0,
-      j = 0;
+      j = 0,
+      replaceBegin = '<strong>',
+      replaceAfter = '</strong>';
 
+    // TODO: test
     this.puzzle1Replace = function(str) {
-      var replaceBegin = '<strong>',
-        replaceAfter = '</strong>';
-
       return replaceBegin + str + replaceAfter;
+    }
+
+    // need to ordered (item1Contain come first)
+    // TODO: test
+    this.puzzle1IsIntersect = function(item1Contain, item2Contain) {
+      var item1 = {
+        start   : item1Contain.offset,
+        end     : item1Contain.offset + item1Contain.length,
+      };
+      var item2 = {
+        start   : item2Contain.offset,
+        end     : item2Contain.offset + item2Contain.length,
+      }
+      var result = (item1.end > item2.start);
+      
+      return result;
     }
 
     if (nArg1 === 1) {
@@ -216,11 +338,13 @@ module.exports = {
       // case 2, 3, 4, 5, 6
       var item1 = arg1[0],
         item2 = arg1[1],
-        item1OccurredIndex = arg2.indexOf(item1),
-        item2OccurredIndex = arg2.indexOf(item2),
-        isContainItem1 = (item1OccurredIndex !== -1),
-        isContainItem2 = (item2OccurredIndex !== -1),
-        intersect = this.getIntersection(item1, item2);
+        item1Contain = this.getContainStr(arg2, item1),
+        item2Contain = this.getContainStr(arg2, item2);
+
+      var item1OccurredIndex = item1Contain.offset,
+        item2OccurredIndex = item2Contain.offset,
+        isContainItem1 = item1Contain.isContain,
+        isContainItem2 = item2Contain.isContain;
 
       // no enough information
       if (item1 === item2) return arg2;
@@ -231,10 +355,18 @@ module.exports = {
           tmp = item1;
           item1 = item2;
           item2 = tmp;
+
+          tmp = item1Contain;
+          item1Contain = item2Contain;
+          item2Contain = tmp;
           
           tmp = item1OccurredIndex;
           item1OccurredIndex = item2OccurredIndex;
           item2OccurredIndex = tmp;
+
+          tmp = isContainItem1;
+          isContainItem1 = isContainItem2;
+          isContainItem2 = tmp;
         }
 
         // case 2, 3, 4
@@ -242,19 +374,25 @@ module.exports = {
           nItem2Occurred = this.getNOccurrences(arg2, item2);
 
         if (nItem1Occurred === 1 && nItem2Occurred === 1) {
-          if (intersect) {
+          var isItem1Item2Intersect = this.puzzle1IsIntersect(item1Contain, item2Contain);
+
+          if (isItem1Item2Intersect) {
             // case 3, 4
-            /*
-            3 | ["abbbbb", "bbbbbc"]  | "xxxxxxabbbbbcxxx"  | "xxxxxx<strong>a<strong>bbbbb</strong>c</strong>xxx"
-            4 | ["abbbbb", "bbb"]     | "xxxxxxabbbbbcxxx"  | "xxxxxx<strong>a<strong>bbb</strong>bb</strong>cxxx"
-            */
-           
-            result = arg2.replace(intersect, this.puzzle1Replace(intersect));
-            console.log(result);
+
+            // TODO: improve logic
+            var find = '',
+              replace = '',
+              intersection = this.getIntersection(item1, item2),
+              intersectionPhases = intersection.sequence;
+
+            find = this.mergeIntersection(item1, item2);
+            replace = this.mergeIntersection(item1, item2, replaceBegin, replaceAfter);
+
+            result = arg2.replace(find, this.puzzle1Replace(replace));
            
           } else {
             // case 2
-            
+
             // split it
             var parts = this.splitAndKeepDelimiter(arg2, item1),
               lastItem = parts[parts.length - 1];
--- test/script.spec.js
+++ test/script.spec.js
@@ -1,11 +1,123 @@
 var chai = require('chai'),
   expect = chai.expect;
 
-var app = require('../js/script.js');
+var app = require('../js/script');
 
-describe('merge2IntersectionString', function() {
-  it('Basic', function() {
+describe('createGrid', function() {
+  before(function() {
+    var arg1 = 0,
+      arg2 = 0,
+      expected = {};
+  });
 
+  it('Empty Grid', function() {
+    arg1 = 0;
+    arg2 = 0;
+    expected = [];
+    expect(app.createGrid(arg1, arg2)).eql(expected);
+  });
+
+  it('Not empty Grid', function() {
+    arg1 = 2;
+    arg2 = 3;
+    expected = [
+      [0, 0, 0],
+      [0, 0, 0]
+    ];
+    expect(app.createGrid(arg1, arg2)).eql(expected);
+  });
+});
+
+describe('replaceLastOccurrence', function() {
+  before(function() {
+    var arg1 = '',
+      arg2 = '',
+      arg3 = '',
+      expected = '';
+  });
+
+  it('Have occurrence', function() {
+    arg1 = 'xxxBOXxxxBOX';
+    arg2 = 'BOX';
+    arg3 = '';
+    expected = 'xxxBOXxxx'
+    expect(app.replaceLastOccurrence(arg1, arg2, arg3)).equal(expected);
+
+    arg1 = 'abc def abc xyz';
+    arg2 = 'abc';
+    arg3 = 'test';
+    expected = 'abc def test xyz'
+    expect(app.replaceLastOccurrence(arg1, arg2, arg3)).equal(expected);
+
+    // case-sensitive
+    arg1 = 'abc ABC abc abc';
+    arg2 = 'ABC';
+    arg3 = 'test';
+    expected = 'abc test abc abc';
+    expect(app.replaceLastOccurrence(arg1, arg2, arg3)).equal(expected);
+  });
+
+  it('Not have occurrence', function() {
+    arg1 = 'abc ABC abc abc';
+    arg2 = 'AbC';
+    arg3 = 'test';
+    expected = 'abc ABC abc abc';
+    expect(app.replaceLastOccurrence(arg1, arg2, arg3)).equal(expected);
+  });
+});
+
+describe('mergeIntersection', function() {
+  before(function() {
+    var arg1 = '',
+      arg2 = '',
+      arg3 = '',
+      arg4 = '',
+      expected = '';
+  });
+
+  it('Arg1 cover arg2', function() {
+    arg1 = 'aabbbb';
+    arg2 = 'bb';
+    expected = 'aabbbb'
+    expect(app.mergeIntersection(arg1, arg2)).equal(expected);
+
+    arg1 = 'aabbbbccc';
+    arg2 = 'bc';
+    expected = 'aabbbbccc'
+    expect(app.mergeIntersection(arg1, arg2)).equal(expected);
+  });
+
+  it('Arg1 not cover arg2', function() {
+    arg1 = 'aabbbbccc';
+    arg2 = 'bccccc';
+    expected = 'aabbbbccccc';
+    expect(app.mergeIntersection(arg1, arg2)).equal(expected);
+  });
+
+  it('Arg1 cover arg2 + emphasize', function() {
+    arg1 = 'abcde';
+    arg2 = 'bc';
+    arg3 = '<a>';
+    arg4 = '<b>';
+    expected = 'a<a>bc<b>de';
+    expect(app.mergeIntersection(arg1, arg2, arg3, arg4)).equal(expected);
+
+    // replace only first occur
+    arg1 = 'aabbbb';
+    arg2 = 'bb';
+    arg3 = '<a>';
+    arg4 = '</a>';
+    expected = 'aa<a>bb</a>bb';
+    expect(app.mergeIntersection(arg1, arg2, arg3, arg4)).equal(expected);
+  });
+
+  it('Arg1 not cover arg2 + emphasize', function() {
+    arg1 = 'xxxxxaaaa';
+    arg2 = 'aaavvv';
+    arg3 = '<W>';
+    arg4 = '<E>';
+    expected = 'xxxxxa<W>aaa<E>vvv';
+    expect(app.mergeIntersection(arg1, arg2, arg3, arg4)).equal(expected);
   });
 });
 
@@ -63,29 +175,219 @@ describe('splitAndKeepDelimiter', function() {
   });
 });
 
-describe('getIntersection', function() {
+describe('getLongestCommonSubstring', function() {
   before(function() {
     var arg1 = '',
       arg2 = '',
-      expected = '';
+      expected = {};
   });
 
-  it('Basic', function() {
-    /*
+  it('Intersect', function() {
     arg1 = 'abbb';
     arg2 = 'bbc';
-    expected = 'bb';
-    expect(app.getIntersection(arg1, arg2)).equal(expected);
-    */
+    expected = {
+      length    : 2,
+      sequence  : 'bb',
+      offset    : 1
+    };
+    expect(app.getLongestCommonSubstring(arg1, arg2)).eql(expected);
 
+    arg1 = 'abbbbb';
+    arg2 = 'bbbbbc';
+    expected = {
+      length    : 5,
+      sequence  : 'bbbbb',
+      offset    : 1
+    };
+    expect(app.getLongestCommonSubstring(arg1, arg2)).eql(expected);
+
+    arg1 = 'bbbbb';
+    arg2 = 'bbb';
+    expected = {
+      length    : 3,
+      sequence  : 'bbb',
+      offset    : 0
+    };
+    expect(app.getLongestCommonSubstring(arg1, arg2)).eql(expected);
+    
     arg1 = 'Here is a quick guide for the next time you reach for your favorite oil and some other topics';
     arg2 = 'favorite oil and some other topics can be based on something blah blah';
-    expected = 'favorite oil and some other topic';
-    expect(app.getIntersection(arg1, arg2)).equal(expected);
+    expected = {
+      length    : 34,
+      sequence  : 'favorite oil and some other topics',
+      offset    : 59
+    };
+    expect(app.getLongestCommonSubstring(arg1, arg2)).eql(expected);
+
+    arg1 = '123456';
+    arg2 = '789511';
+    expected = {
+      length    : 1,
+      sequence  : '1',
+      offset    : 0
+    };
+    expect(app.getLongestCommonSubstring(arg1, arg2)).eql(expected);
+
+    // return first occur
+    arg1 = 'abbbbb';
+    arg2 = 'bbb';
+    expected = {
+      length    : 3,
+      sequence  : 'bbb',
+      offset    : 1
+    };
+    expect(app.getLongestCommonSubstring(arg1, arg2)).eql(expected);
+  });
+
+  it('Not intersect', function() {
+    expected = {
+      length    : 0,
+      sequence  : '',
+      offset    : 0
+    };
+
+    arg1 = '123456';
+    arg2 = '78900';
+    expect(app.getLongestCommonSubstring(arg1, arg2)).eql(expected);
+
+    arg1 = 'qwertyipp';
+    arg2 = 'asdlg;hk';
+    expect(app.getLongestCommonSubstring(arg1, arg2)).eql(expected);
   });
 });
 
-describe('Puzzle1', function() {
+describe('isContainStr', function() {
+  before(function() {
+    var arg1 = '',
+      arg2 = '',
+      expected = false;
+  });
+
+  it('Contain', function() {
+    arg1 = 'qwertyui';
+    arg2 = 'yui';
+    expected = true;
+    expect(app.isContainStr(arg1, arg2)).equal(expected);
+  });
+
+  it('Not contain', function() {
+    arg1 = 'qwertyui';
+    arg2 = 'fgh';
+    expected = false;
+    expect(app.isContainStr(arg1, arg2)).equal(expected);
+  });
+});
+
+describe('getContainStr', function() {
+  before(function() {
+    var arg1 = '',
+      arg2 = '',
+      expected = {};
+  });
+
+  it('Contain', function() {
+    arg1 = 'xxxxaaxx';
+    arg2 = 'aa';
+    expected = {
+      length    : 2,
+      offset    : 4,
+      isContain : true
+    };
+    expect(app.getContainStr(arg1, arg2)).eql(expected);
+  });
+
+  it('Not contain', function() {
+    arg1 = 'xxxxaaxx';
+    arg2 = 'bb';
+    expected = {
+      length    : 2,
+      offset    : -1,
+      isContain : false
+    };
+    expect(app.getContainStr(arg1, arg2)).eql(expected);
+
+    // longer
+    arg1 = 'xxxxaaxx';
+    arg2 = 'xxxxaaxxx';
+    expected = {
+      length    : 9,
+      offset    : -1,
+      isContain : false
+    };
+    expect(app.getContainStr(arg1, arg2)).eql(expected);
+  });
+});
+
+describe('isArray', function() {
+  before(function() {
+    var arg1 = [],
+      expected = false;
+  });
+
+  it('Array', function() {
+    expected = true;
+
+    arg1 = [];
+    expect(app.isArray(arg1)).equal(expected);
+
+    arg1 = [1, 2];
+    expect(app.isArray(arg1)).equal(expected);
+
+    arg1 = ['aa', 'bb'];
+    expect(app.isArray(arg1)).equal(expected);
+  });
+
+  it('Not Array', function() {
+    expected = false;
+
+    // string
+    arg1 = 'qwertyui';
+    expect(app.isArray(arg1, arg2)).equal(expected);
+
+    // number
+    arg1 = 123;
+    expect(app.isArray(arg1, arg2)).equal(expected);
+
+    // object
+    arg1 = {};
+    expect(app.isArray(arg1, arg2)).equal(expected);
+  });
+});
+
+describe('isString', function() {
+  before(function() {
+    var arg1 = [],
+      expected = false;
+  });
+
+  it('String', function() {
+    expected = true;
+
+    arg1 = '12';
+    expect(app.isString(arg1)).equal(expected);
+
+    arg1 = 'aaaaa';
+    expect(app.isString(arg1)).equal(expected);
+  });
+
+  it('Not String', function() {
+    expected = false;
+
+    // array
+    arg1 = [];
+    expect(app.isString(arg1, arg2)).equal(expected);
+
+    // number
+    arg1 = 123;
+    expect(app.isString(arg1, arg2)).equal(expected);
+
+    // object
+    arg1 = {};
+    expect(app.isString(arg1, arg2)).equal(expected);
+  });
+});
+
+describe('Puzzle 1', function() {
   before(function() {
     var arg1 = [],
       arg2 = '',
@@ -158,7 +460,7 @@ describe('Puzzle1', function() {
     arg1 = ['ab11', '22bc'];
     arg2 = 'xxab11xxxx22bcxxx';
     expected = 'xx<strong>ab11</strong>xxxx<strong>22bc</strong>xxx';
-    expect(app.puzzle1(arg1, arg2)).equal(expected);    
+    expect(app.puzzle1(arg1, arg2)).equal(expected);
 
     // switch order
     arg1 = ['22bc', 'ab11'];
@@ -175,30 +477,32 @@ describe('Puzzle1', function() {
 
   // case 3, 4
   it('arg1.length = 2 (found both [each occurrence = 1] and intersect)', function() {
-    /*
     arg1 = ['abbbbb', 'bbbbbc'];
     arg2 = 'xxxxxxabbbbbcxxx';
     expected = 'xxxxxx<strong>a<strong>bbbbb</strong>c</strong>xxx';
     expect(app.puzzle1(arg1, arg2)).equal(expected);
-    */
 
-    /*
+    // replace first occur
     arg1 = ['abbbbb', 'bbb'];
     arg2 = 'xxxxxxabbbbbcxxx';
     expected = 'xxxxxx<strong>a<strong>bbb</strong>bb</strong>cxxx';
     expect(app.puzzle1(arg1, arg2)).equal(expected);
-    */
+    
+    // switch
+    arg1 = ['bbbbbc', 'abbbbb'];
+    arg2 = 'xxxxxxabbbbbcxxx';
+    expected = 'xxxxxx<strong>a<strong>bbbbb</strong>c</strong>xxx';
+    expect(app.puzzle1(arg1, arg2)).equal(expected);
   });
 
   it('arg1.length = 2 (found both [each occurrence >= 1]', function() {
     // no enough information
     // example test case
-    /*
-    arg1 = ['ab', 'cd'];
-    arg2 = 'xxabxxabxxcdxx';
-    expected = 'xxabxxabxxcdxx';
-    expect(app.puzzle1(arg1, arg2)).equal(expected);
-    */
+    // 
+    // arg1 = ['ab', 'cd'];
+    // arg2 = 'xxabxxabxxcdxx';
+    // expected = 'xxabxxabxxcdxx';
+    // expect(app.puzzle1(arg1, arg2)).equal(expected);
   });
 
   // case 5, 6
